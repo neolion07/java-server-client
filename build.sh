de0e1660a912a000602bc354357d25d56552c3fd
neolion07 <neolion.05.08@gmail.com>
2025-02-05 06:02:46 -0400

    Uncommented build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 javac src/server/*.java -d .
-#javac src/client/*.java -d .
+javac src/client/*.java -d .
 
-#echo "java -cp ./lib/postgresql-42.7.4.jar:. com.javaserverclient.server.Main" > run-server.sh
-#chmod 755 run-server.sh
-#echo "java -cp .:. com.javaserverclient.client.Main" > run-client.sh
-#chmod 755 run-client.sh
+echo "java -cp ./lib/postgresql-42.7.4.jar:. com.javaserverclient.server.Main" > run-server.sh
+chmod 755 run-server.sh
+echo "java -cp .:. com.javaserverclient.client.Main" > run-client.sh
+chmod 755 run-client.sh
